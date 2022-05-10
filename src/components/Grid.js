1a023027d42ea210d95909dd821dc12c62c4bc0c
Irissf <iris.seijo@gmail.com>
2022-05-10 11:04:45 +0200

    multi languaje

---
+++ src/components/Grid.js
@@ -0,0 +1,28 @@
+import React from 'react';
+
+const Grid = () => {
+	return (
+		<div className="grid">
+			<div>
+				<img src="https://i.pinimg.com/564x/21/5d/65/215d65bdaababc62cd7b2280ac1f617b.jpg" alt=""/>
+			</div>
+			<div>
+				<img src="https://i.pinimg.com/564x/57/bc/01/57bc01167f4b326d68693d84604ee8c5.jpg" alt=""/>
+			</div>
+			<div>
+				<img src="https://i.pinimg.com/564x/5f/8e/dc/5f8edc033e8ecdbc50eb858e8886d41a.jpg" alt=""/>
+			</div>
+			<div>
+				<img src="https://i.pinimg.com/564x/dd/63/27/dd63277a28d620c13d04b213e28fe945.jpg" alt=""/>
+			</div>
+			<div>
+				<img src="https://i.pinimg.com/564x/8f/6c/98/8f6c98e341913a79feeb8a2a78cd6882.jpg" alt=""/>
+			</div>
+			<div>
+				<img src="https://i.pinimg.com/564x/93/60/56/93605624874a5eb84cf5ebf87f284536.jpg" alt=""/>
+			</div>
+		</div>
+	);
+}
+ 
+export default Grid;
